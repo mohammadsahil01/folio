e9ef92fbe7be1440a2817594b280dc35cb680dfc
mohammadsahil01 <mohammadsahil01@gmail.com>
2024-01-12 18:51:55 +0530

    .

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return ( 
-    <div className="pt-5 px-3 bg-slate-700 text-white flex items-center justify-center">
+    <div className="pt-1 pb-2 px-3 bg-slate-700 text-white flex items-center justify-center">
     Copyright © 2024 Sahil Mohammad. All Rights reserved.    
     </div> );
 }
